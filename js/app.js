383c155fabe0daec8a6842f150774ad8d1450af0
Gunther Cox <gunthercx@gmail.com>
2015-05-30 20:57:23 -0400

    Added Fibonacci Finger Calculator Tool.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
+/* Fibonacci Finger Calculator */
+
+$("#length").on("input change", function() {
+
+    var value = parseFloat($(this).val());
+
+    if (!value) {
+        return;
+    }
+
+    var Phi = (Math.sqrt(5) + 1) / 2;
+    var phi = Phi - 1;
+
+    function fibonacci(n) {
+        if (n >= 0) {
+            return Math.pow(Phi, n) / Math.sqrt(5);
+        } else {
+            return 0;
+        }
+    }
+
+    var phalanx = $(".phalanx");
+
+    var distal = value;
+    var intermediate = distal + fibonacci(distal);
+    var proximal = intermediate + distal;
+    var metacarpal = proximal + intermediate;
+    var carpal = metacarpal + proximal;
+
+    phalanx.find(".distal").text(distal.toFixed(2));
+    phalanx.find(".intermediate").text(intermediate.toFixed(2));
+    phalanx.find(".proximal").text(proximal.toFixed(2));
+    phalanx.find(".metacarpal").text(metacarpal.toFixed(2));
+    phalanx.find(".carpal").text(carpal.toFixed(2));
+
+});
+
+/* Chrome App */
+
 var install_app = document.getElementById("chrome-install");
 var launch_app = document.getElementById("chrome-launch-app");
 
@@ -43,9 +82,9 @@ chrome.webstore.onInstallStageChanged.addListener(function(installStage) {
 ShowChromeAppButtons();
 
 $("#nav").affix({
-  offset: {
-    top: 435
-  }
+    offset: {
+        top: 435
+    }
 });
 
 $("#nav").on("affixed.bs.affix", function() {
