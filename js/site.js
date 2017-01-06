49641fe914bd69ee67de14ebc33aa89cd523085e
Gunther Cox <gunthercx@gmail.com>
2017-01-06 18:52:16 -0500

    Make an index page for specification

--- js/site.js
+++
@@ -1,7 +0,0 @@
-$('.js-show-more').click(function() {
-    $('.js-row-more').toggleClass('open');
-    function reveal() {
-        window.scrollBy(0, 160);
-    }
-    setTimeout(reveal, 100);
-});
